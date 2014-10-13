1f1e634107e97ca3b97171d398a706b64b498adc
Travis Post <travis.post@intel.com>
2014-10-13 10:29:51 -0700

    Simplifying the build steps for initial testing of travis-ci.

--- build.sh
+++ build.sh
@@ -6,10 +6,11 @@ mkdir build
 pushd build
 ../configure --with-platform=../contrib/platform/intel/hillsboro/orcm-nightly-build
 make
-popd
-mkdir static
-pushd static
-../configure --with-platform=../contrib/platform/intel/hillsboro/orcm-nightly-build-static
-make
-popd
+make install
+#popd
+#mkdir static
+#pushd static
+#../configure --with-platform=../contrib/platform/intel/hillsboro/orcm-nightly-build-static
+#make
+#popd
 
