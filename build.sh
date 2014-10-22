fd8f9781785e2acb3d1b7f72969d88cd036eb884
Travis Post <travis.post@intel.com>
2014-10-22 14:40:41 -0700

    debugging

--- build.sh
+++ build.sh
@@ -5,7 +5,9 @@ set -ex
 mkdir build
 pushd build
 export LD_LIBRARY_PATH=/usr/local/lib:${LD_LIBRARY_PATH}
-../configure --with-platform=../contrib/platform/intel/hillsboro/orcm-nightly-build --SIGAR=/usr/local/lib
+#../configure --with-platform=../contrib/platform/intel/hillsboro/orcm-nightly-build
+../configure --help
+exit 1
 make
 make install
 #popd
